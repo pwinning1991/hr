68f7f577cd9d911ccd80db639fd30b2583074adb
Philip Winnington <ftcpjw@yahoo.com>
2019-07-28 17:28:34 -0400

    created add and remove functions

---
+++ funcs.py
@@ -0,0 +1,34 @@
+import pwd
+import string
+import subprocess
+import sys
+
+def add(user_info):
+    print("Adding user '%s'" % user_info['name'])
+    try:
+        subprocess.call([
+            'useradd',
+            '-p',
+            user_info['password'],
+            '-G',
+            _groups_str(user_info),
+            user_info['name'],
+            ])
+    except:
+        print("Failed to add user '%s'" % user_info['name'])
+        sys.exit(1)
+
+def remove(user_info):
+    print("Removing user '%s'" % user_info['name'])
+    try:
+        subprocess.call([
+            'userdel',
+            '-r',
+            user_info['name']
+            ])
+    except:
+        print("Failes to remove user '%s'" % user_info['name'])
+        sys.exit(1)
+
+
+
